8aa0872ee754deb9b418f44797bfc450c9da239c
EspinoAsuna85 <101539228+EspinoAsuna85@users.noreply.github.com>
2023-11-21 07:55:11 +0100

    Update MiniTicketingPlatform.sol

--- MiniTicketingPlatform.sol
+++ MiniTicketingPlatform.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TicketingPlatform is ERC721, Ownable {
+    uint256 public nextTicketId;
+    uint256 public ticketPrice;
+
+    mapping(uint256 => uint256) public ticketEvent;
+    mapping(uint256 => bool) public ticketRedeemed;
+
+    event TicketPurchased(address indexed buyer, uint256 indexed ticketId, uint256 indexed eventId);
+    event TicketRedeemed(address indexed redeemer, uint256 indexed ticketId);
+
+    modifier onlyTicketOwner(uint256 _ticketId) {
+        require(ownerOf(_ticketId) == msg.sender, "Not the ticket owner");
+        _;
+    }
+
+    modifier ticketNotRedeemed(uint256 _ticketId) {
+        require(!ticketRedeemed[_ticketId], "Ticket already redeemed");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol, uint256 _ticketPrice) ERC721(_name, _symbol) {
+        ticketPrice = _ticketPrice;
+    }
+
+}
